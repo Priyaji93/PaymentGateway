d4daf5d5b4c5a58c92a320a53475eb9113a78539
Priyaji93 <priyabajaj3001@gmail.com>
2017-10-08 16:45:17 +0530

    My First Commit

---
+++ payment/src/main/java/com/payment/demo/user/UserAccount.java
@@ -0,0 +1,33 @@
+package com.payment.demo.user;
+
+public class UserAccount {
+
+    private String accounNo;
+    private String amount;
+    private int otp;
+
+    public String getAccounNo() {
+        return accounNo;
+    }
+
+    public void setAccounNo(String accounNo) {
+        this.accounNo = accounNo;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public int getOtp() {
+        return otp;
+    }
+
+    public void setOtp(int otp) {
+        this.otp = otp;
+    }
+}
+
